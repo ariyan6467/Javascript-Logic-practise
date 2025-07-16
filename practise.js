760270f76b26026f91a79016a82acc696c8d9152
nahian jawad <nahianjawad4321@gmail.com>
2025-07-17 04:55:26 +0600

    second commit

---
+++ practise.js
@@ -0,0 +1,45 @@
+
+// function myfactorial(n){
+//     let fac = 1;
+//     for(let i = 1;i <= n;i++){
+//     fac = fac * i;
+//     console.log(fac)
+// }
+//      return fac;
+// };
+
+//  console.log(myfactorial(5));
+
+
+//  let isprime=9;
+
+//  if (isprime > 1 && isprime % 1 == 0 && isprime % isprime == 0){
+//     console.log("this is prime number")
+//  }else {
+//     console.log("not prime");
+//  }
+
+ 
+
+//  function evenoddchecker(start,end){
+//     for(i=start;i<=end;i++){
+//         if (i % 2 === 0){
+//             console.log(i + "is Even number");
+//         }else {
+//             console.log(i + "is odd number")
+//         }
+//     }
+
+//  }
+//  evenoddchecker(1,10);
+
+ let stri ="ariyan";
+ console.log(stri.length)
+ let f = stri.length-1;
+ for(i = 0 ; i <= f ; i++ ){
+    
+    
+ }
+ 
+
+ 
